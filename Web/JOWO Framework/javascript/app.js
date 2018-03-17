e40993fe118f8512246e4f152d46c5bf5a863748
Willington <willingtonortizmaurtua@hotmail.com>
2018-03-17 18:42:24 -0500

    expresiones regulares

--- Web/JOWO Framework/javascript/app.js
+++ Web/JOWO Framework/javascript/app.js
@@ -1,10 +1,18 @@
 window.onload = () => {
-    var sumar = (...parametros) => {
-        var suma = 0;
-        for (var i = 0; i < parametros.length; i++) {
-            suma += parametros[i];
-        }
-        return suma;
+    var ex = new RegExp('ab+c');
+    var entrada = document.querySelector("#entrada");
+    var salida = document.querySelector("#salida");
+    entrada.addEventListener("keydown", () => {
+        encontrar();
+    });
+
+    var encontrar = () => {
+        var textoEntrada = entrada.value;
+        var textoSalida;
+        // textoSalida = ex.match(textoEntrada);
+        textoSalida = textoEntrada.match(/ab+c/);
+        console.log(textoSalida[0]);
+
+        // salida.innerText = textoSalida[0];
     }
-    console.log(sumar(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 }
